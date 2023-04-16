5aade9db9f5538e74c3bad874c1a036040c7c805
jawn-lim <junwen89@hotmail.com>
2023-04-17 00:46:28 +0800

    testaa

--- A-A/A-A-Ingress(SLB)-Egress(NAT)/provider.tf
+++ A-A/A-A-Ingress(SLB)-Egress(NAT)/provider.tf
@@ -1,6 +1,14 @@
+//provider "alicloud" {
+ //       access_key="${var.access_key}"
+  //      secret_key="${var.secret_key}"
+ // 	region  = "ap-southeast-1"
+  //      version = "=1.163.0"
+//}
+
+
 provider "alicloud" {
-        access_key="${var.access_key}"
-        secret_key="${var.secret_key}"
   	region  = "ap-southeast-1"
-        version = "=1.163.0"
+    shared_credentials_file = "/Users/Jawn Lim/.aliyun/config.json"
+    profile                 = "aliprof"
+
 }
